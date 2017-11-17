167b0d8e9ee057a977a93d5cf6b9bc1ce65787a1
西风逍遥游 <sun_xfancy@163.com>
2017-11-16 21:41:33 -0800

    add python3 support

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -13,7 +13,7 @@ class HelloReuseConan(ConanFile):
 
     def build(self):
         cmake = CMake(self.settings)
-        print 'cmake "%s" %s' % (self.conanfile_directory, cmake.command_line)
+        print('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
         self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
